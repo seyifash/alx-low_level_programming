69fc0d01a567492b4add007b7657a6b2bb596aa5
seyifash <fasugbeoluwaseyi@gmail.com>
2023-02-17 15:10:29 -0800

    3 combos

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - A program that prints the combination
+ * Of three single digits
+ * Return: Always 0 (succes)
+ */
+
+int main(void)
+
+{
+	int d, p, q;
+
+	for (d = '0'; d < '9'; d++)
+	{
+	for (p = d + 1; p <= '9'; p++)
+	{
+	for (q = p + 1; q <= '9'; q++)
+	{
+	if ((p != d) != q)
+	{
+	putchar(d);
+	putchar(p);
+	putchar(q);
+	if (d == '7' && p == '8')
+	continue;
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	}
+	}
+	putchar('\n');
+	return (0);
+}
