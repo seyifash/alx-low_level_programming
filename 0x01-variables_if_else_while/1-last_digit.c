cd96a59f747ad342ae824ac7158d0b0f7e00699d
seyifash <fasugbeoluwaseyi@gmail.com>
2023-02-17 10:09:28 -0800

    last digit

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - a program that prints the last digit stored inside avariable
+ * prints if the last digit is greater than 5
+ * prints if the lat digit of n is zero
+ * prints if the last digit of n is less than six and not zero
+ * Return: Alway 0 {success)
+ */
+
+int main(void)
+
+{
+	int n, lastd;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	lastd = n % 10;
+
+	if (lastd > 5)
+	{
+	printf("last digit of %d is %d and is greater than 5\n", n, lastd);
+	}
+	else if (lastd == 0)
+	{
+	printf("last digit of %d is %d and is 0\n", n, lastd);
+	}
+	else if (lastd < 6 && lastd != 0)
+	{
+	printf("last digit of %d is %d and is less than 6 and not 0\n", n, lastd);
+	}
+	return (0);
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -1,1 +1,2 @@
 0- This is a  program will assign a random number to the variable n each time it is executed,and the source code will be completed in order to print whether the number stored in the variable n is positive
+1- Thi is a program that prints the last digit inside a variable
