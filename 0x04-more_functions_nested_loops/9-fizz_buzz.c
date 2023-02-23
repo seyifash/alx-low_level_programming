e4706188d8740a305a0832616907cc2868108528
seyifash <fasugbeoluwaseyi@gmail.com>
2023-02-23 12:40:00 -0800

    fizzbuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - A program that prints fizz buzz
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 1; x <= 100; x++)
+	{
+	if ((x % 3 == 0) && (x % 5 == 0))
+	{
+	printf("fizzbuzz");
+	}
+	else if
+	(x % 3 == 0)
+	{
+	printf("fizz");
+	}
+	else if
+	(x % 5 == 0)
+	{
+	printf("buzz");
+	}
+	else
+	{
+	printf("%d", x);
+	}
+	if (x != 100)
+	{
+	printf(" ");
+	}
+	}
+	printf("\n");
+	return (0);
+}
