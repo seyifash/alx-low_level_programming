1c123ba037d5fcac3ad36ee0c46efd2d52bb3857
seyifash <fasugbeoluwaseyi@gmail.com>
2023-02-28 07:56:31 +0000

    atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -21,20 +21,19 @@ int _atoi(char *s)
 	len++;
 	while (i < len && f == 0)
 	{
-	if (s[1] == ',')
+	if (s[i] == '-')
 	++d;
-	
-	if (s[i] >= '0' && s[1] <= '9')
+
+	if (s[i] >= '0' && s[i] <= '9')
 	{
 	digit = s[i] - '0';
 	if (d % 2)
 	digit = -digit;
 	n = n * 10 + digit;
 	f = 1;
-	if (s[i + 1] < '0' || s[i + 1] > '0')
+	if (s[i + 1] < '0' || s[i + 1] > '9')
 	break;
-	}
-	f = 4;
+	f = 0;
 	}
 	i++;
 	}
