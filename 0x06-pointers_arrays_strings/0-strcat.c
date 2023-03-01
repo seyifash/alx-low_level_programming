ba3bc72ec6b18961c0a1cc2f5791c83cfad6ee1a
seyifash <fasugbeoluwaseyi@gmail.com>
2023-03-01 21:27:42 +0000

    concatenate

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,13 +11,20 @@ char *_strcat(char *dest, char *src)
 {
 	int i'
 	int j;
-	int n;
-
-	for (n = 0; dest[n] != '\0'; n++);
-	for (i = n, j = 0; src[j] != '\0'; i++, j++)
+	
+	i = 0;
+	while (dest[i] != '\0')
+	{
+	i++;
+	}
+	j = 0;
+	while (src[j] != '\0')
 	{
 	dest[i] = src[j];
+	i++;
+	j++;
 	}
 	dest[i] = '\0';
 	return (dest);
+}
 }
