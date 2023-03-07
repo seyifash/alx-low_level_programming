df43a5c01ad92ca313f8afb3d1673533d1c79b72
seyifash <fasugbeoluwaseyi@gmail.com>
2023-03-06 17:06:47 -0800

    task 3

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -5,5 +5,6 @@ char *_memset(char *s, char b, unsigned int n);
 int _putchar(char c);
 char *_strchr(char *s, char c);
 char *_memcpy(char *dest, char *src, unsigned int n);
+unsigned int_strspn(char *s, char *accept);
 
 #endif
