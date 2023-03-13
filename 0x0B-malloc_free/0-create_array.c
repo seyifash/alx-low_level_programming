cdb077fa31de68aa55c3e53d99cf3b10d6f7ae38
seyifash <fasugbeoluwaseyi@gmail.com>
2023-03-13 05:44:07 -0700

    sizes

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -8,12 +8,12 @@
  * Return: pointer to array, NULL if fail
  */
 
-char *create_ array(unsigned int size, char c)
+char *create_array(unsigned int size, char c)
 {
 	char *arr;
 	unsigned int i;
 
-	arr = malloc(sizof(char) * size);
+	arr = malloc(sizeof(char) * size);
 
 	if (size == 0 || arr == NULL)
 		return (NULL);
@@ -21,5 +21,5 @@ char *create_ array(unsigned int size, char c)
 	for (i = 0; i < size; i++)
 		arr[i] = c;
 
-	return(arr);
+	return (arr);
 }
