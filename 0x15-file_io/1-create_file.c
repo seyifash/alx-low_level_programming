2a35f51d9b424c6a2e101c6eaddb0b82714f04e0
seyifash <fasugbeoluwaseyi@gmail.com>
2023-05-08 09:31:07 -0700

    create and append

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,7 +10,7 @@
 int create_file(const char *filename, char *text_content)
 {
 	int op;
-	int len;
+	int len = 0;
 	int wr;
 
 	if (filename == NULL)
--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -8,22 +8,25 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int op;
-	int len;
+	int fd;
+	int len = 0;
 	int wr;
 
 	if (filename == NULL)
 	return (-1);
 
 	if (text_content != NULL)
-	for (len = 0; text_content[len]; len++)
+	{
+	for (len = 0; text_content[len];)
+		len++;
+	}
 
-	op = open(filename, O_WRONLY | O_APPEND);
-	wr = write(op, text_content, len);
+	fd = open(filename, O_WRONLY | O_APPEND);
+	wr = write(fd, text_content, len);
 
-	if (op == -1 || wr == -1)
+	if (fd == -1 || wr == -1)
 	return (-1);
 
-	close(op);
+	close(fd);
 	return (1);
 }
