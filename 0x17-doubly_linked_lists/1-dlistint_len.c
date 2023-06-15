03c447a3714bb47433e25cd7f37b4ba5f593bf00
seyifash <fasugbeoluwaseyi@gmail.com>
2023-06-15 06:45:19 -0700

    prints the number of elements in a double linked list

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_dlistint - prints all elements of a linked list
+ * @h: the head node of the list
+ *
+ * Return: returns the number of elements
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	const dlistint_t *current = h;
+	size_t n = 0;
+
+        while (current != NULL)
+	{
+	n++;
+	current = current->next;
+	}
+	return (n);
+}
