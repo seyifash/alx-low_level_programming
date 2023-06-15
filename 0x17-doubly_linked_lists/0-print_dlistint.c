fb381510552f9fde7f96ac441b1f8aa28afaf4dc
seyifash <fasugbeoluwaseyi@gmail.com>
2023-06-15 06:20:31 -0700

    prints a elements and return n element

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,22 @@
+#include "list.h"
+#include <stdio.h>
+
+/**
+ * print_dlistint - prints all elements of a linked list
+ * @h: the nead node of the list
+ *
+ * Return: returns the number of elements
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	const dlistint_t current = h;
+	size_t n = 0;
+
+	while (current != NULL)
+	{
+	n++;
+	printf("%d\n", current->n);
+	current = curent->next;
+	}
+	return (n);
+}
