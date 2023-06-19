7fa7119bdd31fa62d7412fa49ddc17a3cfb90e7b
seyifash <fasugbeoluwaseyi@gmail.com>
2023-06-19 04:15:46 -0700

    dynamic librabry that contains c functions whic can be called from python

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,49 @@
+#include "main.h"
+/**
+ * add - adds two integers
+ *
+ * Return: returns the sum
+ */
+int add(int a, int b)
+{
+    return a + b;
+}
+/**
+ * sub - subtracts two integers
+ *
+ * Return: returns the difference
+ */
+
+int sub(int a, int b)
+{
+    return a - b;
+}
+/**
+ * mult - multiplies two integers
+ *
+ * Return: returns the product
+ */
+
+int mul(int a, int b)
+{
+    return a * b;
+}
+/**
+ * div - divides two integers
+ *
+ * Return: returns the division
+ */
+
+int div(int a, int b)
+{
+    return a / b;
+}
+/**
+ * mod - returns the remainder after dividing two integers
+ *
+ * Return: returns the remainder
+ */
+int mod(int a, int b)
+{
+    return a % b;
+}
