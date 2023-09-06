4a8c31789bdc50474dee3d97a1c0cb039870815d
seyifash <fasugbeoluwaseyi@gmail.com>
2023-09-06 13:21:23 +0100

    Understanding C program Compilation Process

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - prints a sentence
+ * 
+ * Return: returns 0 on success
+*/
+int main(void)
+{
+    puts("\"Programming is like building a multilingual puzzle");
+    return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - prints a sentence
+ * 
+ * Return: returns 0 on success
+*/
+int main(void)
+{
+    printf("with proper grammar, but the outcome is a piece of art,\n");
+    return (0);
+}
