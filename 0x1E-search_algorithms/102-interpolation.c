0a7eec97cbbeed2b7eacc7c5c2a848d25f04cb73
seyifash <fasugbeoluwaseyi@gmail.com>
2023-10-10 03:54:49 -0700

    interpolation search

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -10,13 +10,13 @@
  */
 int interpolation_search(int *array, size_t size, int value)
 {
-	size_t low;
-	size_t high;
+	size_t low = 0;
+	size_t high = size - 1;
 	size_t pos;
 
 	if (!array)
 		return (-1);
-	for (low = 0, high = size - 1; high >= low;)
+	while ( high >= low)
 	{
 	pos = low + (((double)(high - low) / (array[high] - array[low])) *
 			(value - array[low]));
