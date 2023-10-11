f970015a9b2ae2b370eec5f2d9d403787909688b
seyifash <fasugbeoluwaseyi@gmail.com>
2023-10-11 03:21:36 -0700

    the fix

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -4,6 +4,20 @@
 #include <math.h>
 
 /**
+ * min -  find minimum
+ * @a: first item in comparison
+ * @b: second
+ * Return: minimum
+ */
+size_t min(size_t a, size_t b)
+{
+	if (a <= b)
+		return (a);
+	else
+		return (b);
+}
+
+/**
  * jump_search - jump search algo
  * @array: array to search from
  * @size: size of array
@@ -12,25 +26,25 @@
  */
 int jump_search(int *array, size_t size, int value)
 {
-	size_t a, b = 0;
-	size_t i;
-	size_t f;
+	size_t a = 0;
+	size_t b = sqrt(size);
 
 	if (!array || size == 0)
 		return (-1);
-	i = sqrt(size);
+
+	printf("Value checked array[%lu] = [%d]\n", a, array[a]);
 	while (array[b] < value && b < size)
 	{
 		printf("Value checked array[%lu] = [%d]\n", b, array[b]);
 		a = b;
-		b = b + i;
+		b = b + sqrt(size);
 	}
 	printf("Value found between indexes [%lu] and [%lu]\n", a, b);
-	for (f = a; f <= b && f < size; f++)
+	for (; a <= min(b, size - 1); a++)
 	{
-		printf("Value checked array[%lu] = [%d]\n", f, array[f]);
-		if (array[f] == value)
-			return (f);
+		printf("Value checked array[%lu] = [%d]\n", a, array[a]);
+		if (array[a] == value)
+			return (a);
 	}
 	return (-1);
 }
